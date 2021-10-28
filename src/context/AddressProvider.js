2c61669fdaf0134ebd33a552c7325e7ca5889c85
Eduardo Seije <eduardoseije@gmail.com>
2021-10-28 17:47:40 -0300

    Criação do contexto e configuração do provider para a aplicação

--- src/context/AddressProvider.js
+++ src/context/AddressProvider.js
@@ -1,1 +1,19 @@
-import React, { useState, useEffect, createContext } from 'react';
+import React, { useState, createContext } from 'react';
+
+export const AddressContext = createContext();
+
+const AddressProvider = ({ children }) => {
+  const [address, setAddress] = useState([]);
+  const context = {
+    address,
+    setAddress
+  };
+  return (
+    <AddressContext.Provider value={ context }>
+      { children }
+    </AddressContext.Provider>
+  );
+};
+
+export default AddressProvider;
+
--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AddressProvider from './context/AddressProvider';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <AddressProvider>
+      <App />
+    </AddressProvider>
   </BrowserRouter>,
   document.getElementById('root')
 );
