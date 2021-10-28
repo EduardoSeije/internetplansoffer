cce9e3e4a25e3af5c0197757ac23054634c07c1e
Eduardo Seije <eduardoseije@gmail.com>
2021-10-28 14:20:16 -0300

    Criação dos componentes e paginas necessárias para aplicação

---
+++ src/components/CepInput.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const CepInput = () => {
+  return (
+    <div>
+      <h1>CepInput</h1>
+    </div>
+  )
+}
---
+++ src/pages/internetOffer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const internetOffer = () => {
+  return (
+    <div>
+      <h1>Internet Plan</h1>
+    </div>
+  )
+}
