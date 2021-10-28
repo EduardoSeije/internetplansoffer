4364f1b5c07a87fec9b44183e0b0aead3112102c
Eduardo Seije <eduardoseije@gmail.com>
2021-10-28 19:28:07 -0300

    Implementação das estruturas dos cards através do Map das informações passadas

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,16 +6,27 @@ export const Cards = () => {
     "userData": 
     { "zipcode": "", 
     "offers": 
-    [ { "name": "Oferta 1", "price": 5099, "id": 1, "isSelected": false }, 
-    { "name": "Oferta 2", "price": 10099, "id": 2, "isSelected": false }, 
-    { "name": "Oferta 3", "price": 15099, "id": 3, "isSelected": false } ] } };
-  
-  
+    [ { "name": "Oferta 1", "price": 50.99, "id": 1, "isSelected": false }, 
+    { "name": "Oferta 2", "price": 100.99, "id": 2, "isSelected": false }, 
+    { "name": "Oferta 3", "price": 150.99, "id": 3, "isSelected": false } ] } };
+    
+    const { offers } = cardsData.userData;
+    
     return (
     <div className='cardWapper'>
       <div className='internetCards'>
-        <h1>Cards</h1>
-        {console.log(cardsData.userData.offers)}
+        {offers.map((offer) => {
+         return (
+          <div key={offer.name}>
+            <h3>{offer.name}</h3>
+            <h3>{offer.price.toLocaleString('pt-BR',{
+              style: 'currency',
+              currency: 'BLR'
+            })}</h3>
+            <h3>{offer.isSelected ? <p>Selecionado</p> : <p>Não Selecionado</p>}</h3>
+          </div>   
+         );
+        })}
       </div>
       <PrevButton />    
     </div>
